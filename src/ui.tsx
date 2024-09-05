4ce1111376c7180725b8d5a0a6546b36a75f9db6
UXDerrick <tsormed@gmail.com>
2024-09-05 17:28:13 +0000

    Layout improved

--- src/ui.tsx
+++ src/ui.tsx
@@ -7,6 +7,7 @@ import {
   render,
   Tabs,
   TabsOption,
+  IconArrowLeftRight32,
 } from "@create-figma-plugin/ui";
 import { h, RefObject } from "preact";
 import { useCallback, useEffect, useRef, useState } from "preact/hooks";
@@ -20,18 +21,27 @@ function Plugin() {
   const [imageSrc, setImageSrc] = useState<string | null>(null);
   const { color, palette } = useColorThief(imageSrc as string, {
     format: "hex",
-    colorCount: 6,
+    colorCount: 8,
     quality: 10,
   });
   const containerElementRef: RefObject<HTMLDivElement> = useRef(null);
+  const [showPalette, setShowPalette] = useState<boolean>(false);
 
   const handleImageUpload = useCallback((event: Event) => {
     const file = (event.target as HTMLInputElement).files?.[0];
     if (file) {
       const reader = new FileReader();
-      reader.onload = () => setImageSrc(reader.result as string);
+      reader.onload = () => {
+        setImageSrc(reader.result as string);
+        setShowPalette(true);
+      };
       reader.readAsDataURL(file);
     }
+  }, []);
+
+  const handleBackClick = useCallback(() => {
+    setShowPalette(false);
+    setImageSrc(null);
   }, []);
 
   const handleApplyColorsButtonClick = useCallback(() => {
@@ -70,108 +80,104 @@ function Plugin() {
 
   const renderImageTab = () => (
     <React.Fragment>
-      <div
-        className={styles.uploadContainer}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "10px",
-          height: "120px",
-          marginTop: "10px",
-          cursor: "pointer",
-          border: "2px dashed var(--figma-color-border)",
-          borderRadius: "8px",
-          padding: "20px",
-        }}
-        onClick={() => document.getElementById("fileInput")?.click()}
-      >
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handleImageUpload}
-          className={styles.fileInput}
-          id="fileInput"
-          style={{ display: "none" }}
-        />
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M7 10L12 15L17 10"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M12 15V3"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-        <Text
+      {!showPalette ? (
+        <div
+          className={styles.uploadContainer}
           style={{
-            color: "var(--figma-color-text)",
-            fontSize: "14px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "10px",
+            height: "120px",
             marginTop: "10px",
-            marginBottom: "4px",
+            cursor: "pointer",
+            border: "2px dashed var(--figma-color-border)",
+            borderRadius: "8px",
+            padding: "20px",
           }}
+          onClick={() => document.getElementById("fileInput")?.click()}
         >
-          Click to upload an image
-        </Text>
-        <Text
-          style={{
-            color: "gray",
-            fontSize: "12px",
-            textAlign: "center",
-          }}
-        >
-          Upload an image to generate a color palette
-        </Text>
-      </div>
-      <VerticalSpace space="medium" />
-      {imageSrc && (
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageUpload}
+            className={styles.fileInput}
+            id="fileInput"
+            style={{ display: "none" }}
+          />
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M7 10L12 15L17 10"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M12 15V3"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <Text
+            style={{
+              color: "var(--figma-color-text)",
+              fontSize: "12px",
+              marginTop: "4px",
+              marginBottom: "2px",
+            }}
+          >
+            Click to upload an image
+          </Text>
+          <Text
+            style={{
+              color: "gray",
+              fontSize: "12px",
+              textAlign: "center",
+            }}
+          >
+            Upload an image to generate a color palette
+          </Text>
+        </div>
+      ) : (
         <React.Fragment>
+          <VerticalSpace space="extraSmall" />
           <div
             style={{
               display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              gap: "24px",
-              marginBottom: "10px",
-              marginLeft: "10px",
-              marginRight: "10px",
+              justifyContent: "left",
+              gap: "1fr",
             }}
           >
-            <div className={styles.imagePreview}>
-              <img
-                src={imageSrc}
-                alt="Uploaded image"
-                style={{ width: "100%", height: "auto" }}
-              />
-            </div>
             {palette && (
-              <div style={{ width: "48%" }}>
+              <div style={{ width: "fit-content" }}>
+                <VerticalSpace space="extraSmall" />
                 <Text>
                   <h3
-                    style={{ margin: "0", fontWeight: "bold", color: "gray" }}
+                    style={{
+                      margin: "0",
+                      // fontWeight: "bold",
+                      color: "gray",
+                      marginBottom: "4px",
+                    }}
                   >
-                    Generated Palette:
+                    Colors found:
                   </h3>
                 </Text>
                 <VerticalSpace space="small" />
@@ -179,10 +185,30 @@ function Plugin() {
                   className={styles.palette}
                   style={{
                     display: "grid",
-                    gridTemplateColumns: "repeat(3, 1fr)",
-                    gap: "10px",
+                    gridTemplateColumns: "repeat(9, 1fr)",
+                    gap: "8px",
                   }}
                 >
+                  <div
+                    className={styles.imagePreview}
+                    style={{
+                      width: "40px",
+                      height: "40px",
+                      backgroundColor: "var(--figma-color-background)",
+                      border: "1px solid var(--figma-color-border)",
+                      borderRadius: "8px",
+                    }}
+                  >
+                    <img
+                      src={imageSrc as string}
+                      alt="Uploaded image"
+                      style={{
+                        width: "36px",
+                        height: "36px",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </div>
                   {palette.map((color, index) => (
                     <div
                       key={index}
@@ -194,48 +220,69 @@ function Plugin() {
                         alignItems: "center",
                         justifyContent: "center",
                       }}
-                    >
-                      <span className={styles.colorHex}>{color}</span>
-                    </div>
+                    ></div>
                   ))}
                 </div>
               </div>
             )}
           </div>
-          <VerticalSpace space="large" />
-          <Button
-            fullWidth
-            onClick={handleApplyColorsButtonClick}
-            disabled={!palette}
+          <VerticalSpace space="extraSmall" />
+          <div
+            className="divider"
             style={{
-              marginLeft: "10px",
-              marginRight: "10px",
-              width: "380px",
+              width: "100%",
+              height: "1px",
+              backgroundColor: "var(--figma-color-border)",
+              margin: "0",
+              opacity: "0.2",
             }}
-          >
-            Generate Palette
-          </Button>
+          ></div>
           <VerticalSpace space="small" />
-          <Button
-            fullWidth
-            onClick={handleAddToStylesClick}
-            disabled={!palette}
-            secondary
+          <div
+            className="div"
             style={{
-              marginLeft: "10px",
-              marginRight: "10px",
-              width: "380px",
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              gap: "8px",
             }}
           >
-            Add to Styles
-          </Button>
+            <Button onClick={handleApplyColorsButtonClick} disabled={!palette}>
+              Generate Palette
+            </Button>
+            <Button
+              onClick={handleAddToStylesClick}
+              disabled={!palette}
+              secondary
+              style={{
+                border: "1px solid var(--figma-color-border)",
+              }}
+            >
+              Add to Styles
+            </Button>
+            <Button
+              onClick={handleBackClick}
+              style={{
+                backgroundColor: "transparent",
+                border: "1px solid var(--figma-color-border)",
+                color: "var(--figma-color-text)",
+              }}
+            >
+              Upload another image
+            </Button>
+          </div>
         </React.Fragment>
       )}
     </React.Fragment>
   );
 
   const renderWebsiteTab = () => (
-    <div style={{ marginTop: "10px" }}>
+    <div
+      style={{
+        marginTop: "10px",
+        height: "120px",
+      }}
+    >
       <Text style={{ textAlign: "center", color: "gray" }}>
         Website tab content coming soon...
       </Text>
@@ -243,7 +290,12 @@ function Plugin() {
   );
 
   const renderRandomTab = () => (
-    <div style={{ marginTop: "10px" }}>
+    <div
+      style={{
+        marginTop: "10px",
+        height: "120px",
+      }}
+    >
       <Text style={{ textAlign: "center", color: "gray" }}>
         Random tab content coming soon...
       </Text>
@@ -274,11 +326,9 @@ function Plugin() {
     <Container
       ref={containerElementRef}
       space="extraSmall"
-      style={
-        {
-          // backgroundColor: "#fff",
-        }
-      }
+      style={{
+        paddingBottom: "8px",
+      }}
     >
       <Tabs
         options={tabOptions}
@@ -288,6 +338,21 @@ function Plugin() {
       {activeTab === "image"}
       {activeTab === "website"}
       {activeTab === "random"}
+      <Button
+        onClick={() => {
+          window.open("https://buymeacoffee.com/uxderrick", "_blank");
+        }}
+        style={{
+          backgroundColor: "transparent",
+          border: "none",
+          color: "gray",
+          padding: "0",
+          margin: "0",
+          textDecoration: "underline",
+        }}
+      >
+        Buy me a coffee
+      </Button>
     </Container>
   );
 }
